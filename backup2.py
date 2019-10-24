d2cffad63bf689d9ccd50ab324445a9aca98f629
JF <56718718+Fred-Jeuf@users.noreply.github.com>
2019-10-24 02:17:55 +0100

    Update backup2.py

--- backup2.py
+++ backup2.py
@@ -4,9 +4,9 @@ from os import path
 
 def main():
     # make a duplicate of an existing file
-	if path.exists("guru99.txt"):
+	if path.exists("c:/Users/JF/Desktop/Source/text.txt"):
     # get the path to the file in the current directory
-        src = path.realpath("guru99.txt");
+        src = path.realpath("c:/Users/JF/Desktop/Source/text.txt")
     
 	#seperate the path from the filter
 	head, tail = path.split(src)
